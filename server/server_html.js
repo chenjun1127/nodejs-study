0cc9cb7c14525f4324fde4372984b084077e76c1
chenjun <chenjun@oeasy.com>
2017-12-26 18:00:47 +0800

    update

--- server/server_html.js
+++ server/server_html.js
@@ -1,20 +1,27 @@
-var http = require('http')
-var fs = require('fs')
-http.createServer(function(req, res) {
-	if (req.url == '/') {
-		fs.readFile('./titles.json', function(err, data) {
-			if (err) console.log(err.stack)
-			var titles = JSON.parse(data.toString());
-			var output = '<html><head></head><body>' + '<h1>Latest Posts</h1>' + '<ul>';
-			for (var i in titles) {
-				output += '<li>' + titles[i].title + '</li>';
-			}
-			output += '<ul></body></html>';
-			res.writeHead(200, {
-				'Content-Type': 'text/html'
-			});
-			res.end(output);
-		})
-	}
-}).listen(3100, '127.0.0.1')
-console.log('Server running at http://127.0.0.1:3100/');
+/*
+ * @Author: chenjun
+ * @Date:   2017-12-26 17:41:30
+ * @Last Modified by:   chenjun
+ * @Last Modified time: 2017-12-26 17:56:17
+ */
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    let pathname = url.parse(req.url).pathname;
+    fs.readFile(pathname.substring(1), (err, data) => {
+        if (err) {
+            console.log(err);
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html,charset=utf-8' });
+            res.write(data.toString());
+        }
+        res.end();
+    })
+})
+
+server.listen(3100, '127.0.0.1', () => {
+    console.log('Server running at http://127.0.0.1:3100/');
+})
